da4be02e53d0469fa387560578fd4b4e752b42fb
RoGoSo <iamrgs@qq.com>
2018-09-02 22:44:28 +0800

    add augmentation

--- model.py
+++ model.py
@@ -1,15 +1,12 @@
-#coding:utf-8
-# author: Gaosheng Ren, Call me Marks will be cool.
+# coding:utf-8
+# author: Gaosheng Ren, Marks as well.
 # iamrgs@foxmail.com
 
-from mxnet import gluon
 import mxnet.gluon.nn as nn
 from mxnet import image
 import mxnet as mx
 from mxnet import nd
 import numpy as np
-from mxnet.gluon.block import HybridBlock
-
 
 
 # 通道shuffle
@@ -29,7 +26,7 @@ class shuffle_channels(nn.HybridBlock):
 
 
 # ShuffleNet stride=1
-class ShuffleNetUnitA(HybridBlock):
+class ShuffleNetUnitA(nn.HybridBlock):
     """ShuffleNet unit for stride=1"""
     def __init__(self, in_channels, out_channels, groups=3, **kwargs):
         super(ShuffleNetUnitA, self).__init__()
@@ -60,7 +57,7 @@ class ShuffleNetUnitA(HybridBlock):
         return out
 
 # strike=2的基本单元
-class ShuffleNetUnitB(HybridBlock):
+class ShuffleNetUnitB(nn.HybridBlock):
     """ShuffleNet unit for stride=2"""
     def __init__(self, in_channels, out_channels, groups=3, **kwargs):
         super(ShuffleNetUnitB, self).__init__(**kwargs)
@@ -99,49 +96,62 @@ class RELU6(nn.HybridBlock):
         return F.clip(x, 0, 6, name="fwd")
 
 # g=3的ShuffleNet的实现
-class ShuffleNet(HybridBlock):
-    """ShuffleNet for groups=3"""
+class ShuffleNet(nn.HybridBlock):
+    """
+    ShuffleNet for groups=3;
+    param:
+    - groups = 3;
+    - num_classes: The ouput classes, default is 10;
+    """
     def __init__(self, groups=3, num_classes=10, **kwargs):
         super(ShuffleNet, self).__init__(**kwargs)
-        
-        with self.name_scope():
-#             self.conv1 = nn.Conv2D(channels=24, kernel_size=3, stride=2, padding=1)
-#             self.max_pool2d = nn.MaxPool2D(pool_size=(3, 3), strides=(2,2), padding=1)
-            
-            self.features = nn.HybridSequential(prefix='shufflenet_')
-            with self.features.name_scope():
-                
-                self.features.add(nn.Conv2D(channels=24, kernel_size=3, strides=2, padding=1))
-                self.features.add(nn.MaxPool2D(pool_size=(3, 3), strides=(2,2), padding=1))
+        self.num_classes = num_classes
+        with self.name_scope():         
+            self.features = nn.HybridSequential(prefix='')
 
-                #stage2_seq
-                self.features.add(ShuffleNetUnitB(24,240, groups=3))#24,240
-                for i in range(3):
-                    self.features.add(ShuffleNetUnitA(240, 240, groups=3)) #240, 240
+            self.features.add(nn.Conv2D(channels=24, kernel_size=3, strides=2, padding=1))
+            self.features.add(nn.MaxPool2D(pool_size=(3, 3), strides=(2,2), padding=1))
 
-                #stage3_seq 
-                self.features.add(ShuffleNetUnitB(240, 480, groups=3))#240, 480
-                for i in range(7):
-                    self.features.add(ShuffleNetUnitA(480, 480, groups=3))#480, 480, 
+            #stage2_seq
+            self.features.add(ShuffleNetUnitB(24,240, groups=3))#24,240
+            for _ in range(3):
+                self.features.add(ShuffleNetUnitA(240, 240, groups=3)) #240, 240
 
-                #stage4_seq
-                self.features.add(ShuffleNetUnitB(480, 960, groups=3))#480, 960,
-                for i in range(3):
-                    self.features.add(ShuffleNetUnitA(960, 960, groups=3))#960, 960
+            #stage3_seq 
+            self.features.add(ShuffleNetUnitB(240, 480, groups=3))#240, 480
+            for _ in range(7):
+                self.features.add(ShuffleNetUnitA(480, 480, groups=3))#480, 480, 
 
-                self.features.add(nn.GlobalAvgPool2D())
-                self.features.add(nn.Dense(num_classes))#, activation="linear" means a(x) = x
-#              nn.Activation(activation='relu')
-#             self.avg_pool = nn.AvgPool2D(pool_size=(7, 7))                
-#             self.fc = nn.Dense(num_classes, activation="linear")
+            #stage4_seq
+            self.features.add(ShuffleNetUnitB(480, 960, groups=3))#480, 960,
+            for _ in range(3):
+                self.features.add(ShuffleNetUnitA(960, 960, groups=3))#960, 960
+
+            self.features.add(nn.GlobalAvgPool2D())
+
+            self.output = nn.Dense(self.num_classes)
 
     def hybrid_forward(self, F, x):
-#         net = self.conv1(x)
-#         net = self.max_pool2d(net)
-        net = self.features(x)
-#         net = self.avg_pool(net)
-#         net = F.avg_pool2d(net, 7)
-#         net = net.view(net.size(0), -1)
-#         net = self.fc(net)
-        logits = F.softmax(net)
-        return logits
+        x = self.features(x)
+        x = self.output(x)
+        x = F.softmax(x)
+        return x
+
+
+## self test
+if __name__ == "__main__":
+    net = ShuffleNet()
+    net.initialize()
+
+    xx = nd.zeros((32, 3, 224, 224))
+    y = net(xx)
+    print (net)
+    print (y)
+    # n = net.features
+    # print (n)
+    print ("Build The ShuffleNet Success!")
+
+    ## visualize for jupyter notebook
+    # x = mx.sym.var('data')
+    # sym = net(x)
+    # mx.viz.plot_network(sym).view()
--- utils/dataLoader.py
+++ utils/dataLoader.py
@@ -1,73 +1,89 @@
 # conding:utf-8
-
 # author: Gaosheng Ren, Marks
 # iamrgs@foxmail.com
 
-import mxnet.gluon.nn as nn
+from mxnet.gluon.data import dataset
 import mxnet as mx
-import mxnet.ndarray as nd
+
 import numpy as np
-from mxnet.gluon.block import HybridBlock
+import os
+import collections
+
 import cv2
 
-rgb_mean = nd.array([0.485, 0.456, 0.406])
-rgb_std = nd.array([0.229, 0.224, 0.225])
-
-# normalize
-def normalize_image(data):
+## dataLoder for classify
+class DatasetForClassify(dataset.Dataset):
     """
-    Info: Data normalize.
+    A dataset for loading images  stored as `xyz.jpg` and `xyz_mask.png`.
+
+    Parameters
+    ----------
+    root : str
+        Path to root directory.
+    transform : callable, default None
+        A function that takes data and label and transforms them:
+    ::
+        transform = lambda data, label: (data.astype(np.float32)/255, label)
+    
+    input_size: input_size format = (W, H), NOT as MXnet image (H,W)!
+    
+    Note: If you want to show the images that youeant to read, Please Commented the 
+    color normalize function!
     """
-    return (data.astype('float32') / 255 - rgb_mean) / rgb_std
+    def __init__(self, root, transform=None, input_size=(360,640)):
+        self._root = os.path.expanduser(root)
+        self._transform = transform
+        self._exts = ['.jpg', '.jpeg', '.png']
+        self._list_images(self._root)
+        self.mean = mx.nd.array([0.485, 0.456, 0.406])
+        self.std = mx.nd.array([0.229, 0.224, 0.225])
+        self._input_size = input_size
+#         self._resize = isResize
+    
+    def _list_images(self, root):
+        images = collections.defaultdict(dict)
+        for filename in sorted(os.listdir(root)):
+            name, ext = os.path.splitext(filename)
+            mask_flag = name.endswith("_mask")
+            if ext.lower() not in self._exts:
+                continue
+            if not mask_flag:
+                images[name]["base"] = filename
+            else:
+                name = name[:-5] # to remove '_mask'
+                images[name]["mask"] = filename
+        self._image_list = list(images.values())
 
-# resize
-class Resize:
-    def __init__(self, w, h):
-        self.w = w
-        self.h = h
-        
-    def __call__(self, img, lbl):
-        img = cv2.resize(img, (w,h), 0, 0, cv2.INTER_CUBIC)
-        lbl = cv2.resize(lbl, (w,h), 0, 0, cv2.INTER_NEAREST)
-        
-        return img, lbl
+    def __getitem__(self, idx):
+        assert 'base' in self._image_list[idx], "Couldn't find base image for: " + self._image_list[idx]["mask"]
+        #read image
+        base_filepath = os.path.join(self._root, self._image_list[idx]["base"])
+        base = mx.image.imread(base_filepath)
+#         base = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#         if self._resize:
+#             base = base.asnumpy()
+#             base = cv2.resize(base,self._input_size)
+#             base = nd.array(base)
+#         base = color_augmentation(base)
+#         base = image.color_normalize(base, self.mean, self.std)
+# #         base = normalize_image(base)
+#         base = base.transpose((2,0,1))
+    
+        #read mask
+        assert 'mask' in self._image_list[idx], "Couldn't find mask image for: " + self._image_list[idx]["base"]
+        mask_filepath = os.path.join(self._root, self._image_list[idx]["mask"])
+        mask = mx.image.imread(mask_filepath)
+        mask = mask[:,:,0]
+#         if self._resize:
+#             mask = mask.asnumpy()
+#             mask = cv2.resize(mask,self._input_size,interpolation=cv2.INTER_NEAREST)
+            
+#         mask = mask.astype('int32')
+#         mask = nd.array(mask)
+        if self._transform is not None:
+            return self._transform(base, mask)
+        else:
+            return base, mask
 
-
-class ToNDArray():
-    def __call__(self, img, lbl):
-        img = mx.nd.array(img) #TODO: dtype
-        lbl = mx.nd.array(lbl) #TODO: dtype
-        
-        return img, lbl
-
-# 颜色与亮度增强
-class Color_augmentation():
-    def __call__(self, img, lbl):
-        aug = mx.image.BrightnessJitterAug(brightness=0.4)
-        aug_base = aug(img.astype('float32'))
-        aug_base = aug_base.clip(0,255)
-        # Add more color augmentations here...
-        return img, lbl
-
-# 归一化
-class Normalize():
-    def __init__(self, mean, std):
-        self.mean = mean
-        self.std = std
-
-    def __call__(self, img, lbl):
-        img = mx.image.color_normalize(img.astype('float32'), self.mean, self.std)
-        img = mx.nd.transpose(img, (2, 0, 1))
-        lbl = lbl.astype('int32').asnumpy()
-        lbl = nd.array(lbl)
-        return img, lbl
-
-#组合增强函数
-class Compose:
-    def __init__(self, trans):
-        self.trans = trans
-
-    def __call__(self, img, lbl):
-        for t in self.trans:
-            img, lbl = t(img, lbl)
-        return img, lbl
+    def __len__(self):
+        return len(self._image_list)
